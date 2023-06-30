835697ddb407e6292e62aa6542ad5a7f3eef56c8
olzhy <olzhy@qq.com>
2023-06-30 18:37:39 +0800

    feed

--- src/test/java/com/boyouquan/service/RSSReaderServiceImplTest.java
+++ src/test/java/com/boyouquan/service/RSSReaderServiceImplTest.java
@@ -1,18 +1,32 @@
 package com.boyouquan.service;
 
+import com.boyouquan.model.BlogPost;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class RSSReaderServiceImplTest {
 
     @Autowired
     private RSSReaderService rssReaderService;
+    @Autowired
+    private BlogPostService blogPostService;
 
     @Test
     public void testRead() {
-        rssReaderService.read("");
+        List<BlogPost> blogPosts = rssReaderService.read("https://owlswims.com/feed/");
+//        save(blogPosts);
+    }
+
+    private void save(List<BlogPost> blogPosts) {
+        if (null != blogPosts) {
+            for (BlogPost blogPost: blogPosts) {
+                blogPostService.saveBlogPost(blogPost);
+            }
+        }
     }
 
 }
