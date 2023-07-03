9d4cd36f71dc0ecb1fb8cc636d79a15373ef22b1
olzhy <olzhy@qq.com>
2023-07-03 15:10:47 +0800

    home controller and about

--- src/main/java/com/boyouquan/controller/HomeController.java
+++ src/main/java/com/boyouquan/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.boyouquan.model.BlogPost;
 import com.boyouquan.service.BlogAccessService;
 import com.boyouquan.service.BlogPostService;
 import com.boyouquan.util.Pagination;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @Controller
-@RequestMapping("/home")
+@RequestMapping("")
 public class HomeController {
 
     @Autowired
@@ -22,19 +25,28 @@ public class HomeController {
     private BlogAccessService blogAccessService;
 
     @GetMapping("")
-    public String index(Model model) {
-        return index(1, model);
+    public void index(HttpServletResponse response) {
+        try {
+            response.sendRedirect("/home");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @GetMapping("/page/{page}")
-    public String index(@PathVariable("page") int page, Model model) {
+    @GetMapping("/home")
+    public String home(Model model) {
+        return homeWithPagination(1, model);
+    }
+
+    @GetMapping("/home/page/{page}")
+    public String homeWithPagination(@PathVariable("page") int page, Model model) {
         Pagination<BlogPost> pagination = blogPostService.listBlogPosts(page, CommonConstants.DEFAULT_PAGE_SIZE);
 
         model.addAttribute("pagination", pagination);
         model.addAttribute("totalBlogs", blogPostService.countBlogs());
         model.addAttribute("totalBlogPosts", blogPostService.countPosts());
         model.addAttribute("accessTotal", blogAccessService.totalCount());
-        return "index";
+        return "home";
     }
 
 }
