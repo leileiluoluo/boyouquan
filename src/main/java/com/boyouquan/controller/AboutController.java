176f706f508f3cd4d518d04288634c74b9c1098a
olzhy <olzhy@qq.com>
2023-07-03 12:34:32 +0800

    parameterize fragments

---
+++ src/main/java/com/boyouquan/controller/AboutController.java
@@ -0,0 +1,17 @@
+package com.boyouquan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/about")
+public class AboutController {
+
+    @GetMapping("")
+    public String about(Model model) {
+        return "about/about";
+    }
+
+}
