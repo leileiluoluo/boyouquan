c7d521f67f344cd29c424c96bc5f6da9941f482d
olzhy <olzhy@qq.com>
2023-07-03 09:19:38 +0800

    use fragments

---
+++ src/main/resources/static/assets/js/theme.js
@@ -0,0 +1,7 @@
+if (localStorage.getItem("pref-theme") === "dark") {
+document.body.classList.add("dark");
+} else if (localStorage.getItem("pref-theme") === "light") {
+document.body.classList.remove("dark");
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+document.body.classList.add("dark");
+}
