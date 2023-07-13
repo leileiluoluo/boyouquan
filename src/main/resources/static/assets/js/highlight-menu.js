584b50dab100be067a20fc572964e7ff7ef4d112
olzhy <olzhy@qq.com>
2023-07-14 06:28:29 +0800

    css

--- src/main/resources/static/assets/js/highlight-menu.js
+++ src/main/resources/static/assets/js/highlight-menu.js
@@ -3,7 +3,7 @@ var currentPath = currentURL.split('?')[0];
 var menuItems = document.getElementById('menu').getElementsByTagName('a');
 for (var i = 0; i < menuItems.length; i++) {
     var item = menuItems[i];
-    if (currentPath == item.href) {
+    if (currentPath.startsWith(item.href)) {
         item.classList.add("active");
     }
 }
