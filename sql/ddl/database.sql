0286c3a0c94b321c2c121951e68ecca67147cecb
olzhy <olzhy@qq.com>
2023-07-22 08:03:29 +0800

    sql: utf8mb4_general_ci

--- sql/ddl/database.sql
+++ sql/ddl/database.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE `boyouquan-dev` DEFAULT CHARSET utf8mb4 COLLATE utf8_general_ci;
-CREATE DATABASE `boyouquan` DEFAULT CHARSET utf8mb4 COLLATE utf8_general_ci;
+CREATE DATABASE `boyouquan-dev` DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE `boyouquan` DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
