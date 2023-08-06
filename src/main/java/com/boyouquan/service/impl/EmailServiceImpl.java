73682519c68d934756e1e49e1e9e822e7dcfd8dd
olzhy <olzhy@qq.com>
2023-08-06 10:52:27 +0800

    gravatar & email

--- src/main/java/com/boyouquan/service/impl/EmailServiceImpl.java
+++ src/main/java/com/boyouquan/service/impl/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.boyouquan.service.impl;
 
 import com.boyouquan.config.BoYouQuanConfig;
+import com.boyouquan.constant.CommonConstants;
 import com.boyouquan.model.Blog;
 import com.boyouquan.model.BlogRequest;
 import com.boyouquan.model.BlogRequestInfo;
@@ -94,22 +95,24 @@ public class EmailServiceImpl implements EmailService {
 
         if (null != blogRequest) {
             String adminEmail = blogRequest.getAdminEmail();
-            String subject = String.format("[博友圈] 恭喜您！您的博客「%s」已被博友圈收录！", blogRequest.getName());
+            if (!CommonConstants.FAKE_BLOG_ADMIN_EMAIL.equals(adminEmail)) {
+                String subject = String.format("[博友圈] 恭喜您！您的博客「%s」已被博友圈收录！", blogRequest.getName());
 
-            Context context = new Context();
-            BlogRequestInfo blogRequestInfo = new BlogRequestInfo();
-            BeanUtils.copyProperties(blogRequest, blogRequestInfo);
-            Blog blog = blogService.getByRSSAddress(blogRequest.getRssAddress());
-            if (null != blog) {
-                blogRequestInfo.setDomainName(blog.getDomainName());
+                Context context = new Context();
+                BlogRequestInfo blogRequestInfo = new BlogRequestInfo();
+                BeanUtils.copyProperties(blogRequest, blogRequestInfo);
+                Blog blog = blogService.getByRSSAddress(blogRequest.getRssAddress());
+                if (null != blog) {
+                    blogRequestInfo.setDomainName(blog.getDomainName());
+                }
+
+                context.setVariable("blogRequestInfo", blogRequestInfo);
+
+                String text = templateEngine.process("email/blog_request_template_collected", context);
+
+                // send
+                send(adminEmail, subject, text, true);
             }
-
-            context.setVariable("blogRequestInfo", blogRequestInfo);
-
-            String text = templateEngine.process("email/blog_request_template_collected", context);
-
-            // send
-            send(adminEmail, subject, text, true);
         }
     }
 
