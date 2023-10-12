895cb36571ffc4e9ea270a9423ce538667c4907a
LeiLei Yan <olzhy@qq.com>
2023-10-12 11:02:31 +0800

    export import

--- sql/export-import/data-export-import.sql
+++ sql/export-import/data-export-import.sql
@@ -2,7 +2,7 @@
 -- mysqldump -h 172.19.0.2 -P 3306 -u root -p --set-gtid-purged=OFF boyouquan-dev > /tmp/boyouquan-dev.sql
 
 -- only export data
-mysqldump -u root -p -t boyouquan > /tmp/boyouquan.sql
+mysqldump --complete-insert -u root -p -t boyouquan > /tmp/boyouquan.sql
 
 -- import
 mysql -u root -p boyouquan < /tmp/boyouquan.sql
