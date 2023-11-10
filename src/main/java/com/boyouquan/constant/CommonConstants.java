b139c0d7c99fadaf4481dc40860857a718272104
root <olzhy@qq.com>
2023-11-10 14:39:27 +0800

    new gravatar address

--- src/main/java/com/boyouquan/constant/CommonConstants.java
+++ src/main/java/com/boyouquan/constant/CommonConstants.java
@@ -18,7 +18,7 @@ public class CommonConstants {
     public static final String BLOG_LIST_ADDRESS = "/blogs";
     public static final String BLOG_ITEM_ADDRESS_PATTERN = "/blogs?keyword=%s";
 
-    public static final String GRAVATAR_SOURCE_ADDRESS = "https://cravatar.cn/avatar/%s?s=%d";
+    public static final String GRAVATAR_SOURCE_ADDRESS = "https://dn-qiniu-avatar.qbox.me/avatar/%s?s=%d";
     public static final String GRAVATAR_STORE_FOLDER = "gravatar/%s/";
     public static final String GRAVATAR_DEFAULT_IMAGE_MD5_EMAIL = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
     public static final int GRAVATAR_IMAGE_SMALL_SIZE = 20;
@@ -46,3 +46,4 @@ public class CommonConstants {
     public static final int SITEMAP_LATEST_POST_FETCH_SIZE = 100;
 
 }
+
