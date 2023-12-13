e9b68ebe26152d9c1560c9a4a2822d2282663bcb
LeiLei Yan <olzhy@qq.com>
2023-12-13 15:12:01 +0800

    ddl

--- sql/ddl/post.sql
+++ sql/ddl/post.sql
@@ -6,8 +6,8 @@ CREATE TABLE post (
     description VARCHAR(1000),
     published_at TIMESTAMP NOT NULL DEFAULT '2023-07-01 00:00:00',
     draft BOOLEAN DEFAULT FALSE,
-    deleted BOOLEAN DEFAULT FALSE,
-    recommended BOOLEAN DEFAULT FALSE
+    recommended BOOLEAN DEFAULT FALSE,
+    deleted BOOLEAN DEFAULT FALSE
 );
 
 CREATE INDEX idx_post_domain_name ON post (blog_domain_name);
