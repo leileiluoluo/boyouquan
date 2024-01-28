5f9d6df0064434cf4b1899c33495011ddf6f90eb
Jane <Jane@larrys-MacBook-Pro.local>
2024-01-28 15:50:25 +0800

    cors support

--- src/main/java/com/boyouquan/controller/FeedController.java
+++ src/main/java/com/boyouquan/controller/FeedController.java
@@ -5,6 +5,7 @@ import com.boyouquan.service.FeedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,7 @@ public class FeedController {
     @Autowired
     private FeedService feedService;
 
+    @CrossOrigin("*")
     @GetMapping(value = "/feed.xml", produces = MediaType.TEXT_XML_VALUE)
     @ResponseBody
     public String feed(@RequestParam(value = "sort", required = false, defaultValue = "recommended") String sort) {
