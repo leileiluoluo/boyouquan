0874194fb787bc61fdf19c7fc2f4553b27c72137
LeiLei Yan <olzhy@qq.com>
2024-03-31 11:35:40 +0800

    planet shuttle

--- src/main/resources/static/assets/js/planet-shuttle/go.js
+++ src/main/resources/static/assets/js/planet-shuttle/go.js
@@ -2,4 +2,4 @@ setTimeout( function() {
     var a = document.getElementById('shuttle');
     var address = a.attributes['href'].nodeValue;
     window.location.href = address;
-}, 5 * 1000 );
+}, 3 * 1000 );
