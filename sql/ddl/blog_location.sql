93053afc07fbf0ada9f5b00fde514627ea9b3558
Jane <Jane@larrys-MacBook-Pro.local>
2024-04-12 19:20:42 +0800

    blog location

--- sql/ddl/blog_location.sql
+++ sql/ddl/blog_location.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS blog_location;
 CREATE TABLE blog_location (
     domain_name VARCHAR(100) PRIMARY KEY,
     location VARCHAR(100) NOT NULL,
-    isp VARCHAR(200) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT '2023-07-01 00:00:00',
     updated_at TIMESTAMP NOT NULL DEFAULT '2023-07-01 00:00:00',
     deleted BOOLEAN DEFAULT FALSE
