75d9ce5aeb782178a51f70682918a0e685bc5e22
LeiLei Yan <olzhy@qq.com>
2024-05-14 09:27:25 +0800

    tongji

--- src/main/resources/static/assets/js/tongji.js
+++ src/main/resources/static/assets/js/tongji.js
@@ -1,7 +1,7 @@
-//var _hmt = _hmt || [];
-//(function() {
-//  var hm = document.createElement("script");
-//  hm.src = "https://hm.baidu.com/hm.js?48aad127b8a92eec5678b1f58041a89f";
-//  var s = document.getElementsByTagName("script")[0];
-//  s.parentNode.insertBefore(hm, s);
-//})();
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?48aad127b8a92eec5678b1f58041a89f";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
