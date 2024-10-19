c965a03358113e743804c931d2b2143a7c85b272
Jane <Jane@larrys-MacBook-Pro.local>
2024-10-19 08:14:27 +0800

    api: blog add

--- src/main/java/com/boyouquan/controller/BlogRequestsRestController.java
+++ src/main/java/com/boyouquan/controller/BlogRequestsRestController.java
@@ -1,7 +1,9 @@
 package com.boyouquan.controller;
 
 import com.boyouquan.constant.CommonConstants;
+import com.boyouquan.helper.BlogRequestFormHelper;
 import com.boyouquan.model.BlogRequest;
+import com.boyouquan.model.BlogRequestForm;
 import com.boyouquan.model.BlogRequestInfo;
 import com.boyouquan.service.BlogRequestService;
 import com.boyouquan.util.Pagination;
@@ -9,12 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/api/blog-requests")
 public class BlogRequestsRestController {
 
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    @Autowired
+    private BlogRequestFormHelper blogRequestFormHelper;
     @Autowired
     private BlogRequestService blogRequestService;
 
@@ -37,6 +47,38 @@ public class BlogRequestsRestController {
     @GetMapping("/{id}")
     public BlogRequestInfo getBlogRequestById(@PathVariable("id") Long id) {
         return blogRequestService.getBlogRequestInfoById(id);
+    }
+
+    @PostMapping("")
+    public Map<String, Object> addBlogRequest(@RequestBody BlogRequestForm blogRequestForm) {
+        Map<String, Object> result = new HashMap<>();
+        Map<String, String> error = blogRequestFormHelper.paramsValidation(blogRequestForm);
+        if (null != error) {
+            result.put("status", "error");
+            result.put("message", error);
+            return result;
+        }
+
+        // params validation
+        if (null != blogRequestService.getByRssAddress(blogRequestForm.getRssAddress())) {
+            result.put("status", "error");
+            result.put("message", "您要提交的博客已存在！");
+            return result;
+        }
+
+        // submit
+        BlogRequest blogRequest = new BlogRequest();
+        blogRequest.setName(blogRequestForm.getName().trim());
+        blogRequest.setDescription(blogRequestForm.getDescription().trim());
+        blogRequest.setRssAddress(blogRequestForm.getRssAddress().trim());
+        blogRequest.setAdminEmail(blogRequestForm.getAdminEmail().trim());
+        blogRequest.setSelfSubmitted(true);
+        blogRequestService.submit(blogRequest);
+
+        executorService.execute(() -> blogRequestService.processNewRequest(blogRequest.getRssAddress()));
+
+        result.put("status", "success");
+        return result;
     }
 
 }
