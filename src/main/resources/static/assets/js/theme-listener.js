c1244ae8b08acc65cb278a26d4e936e5917aca84
Jane <Jane@larrys-MacBook-Pro.local>
2024-10-26 20:29:59 +0800

    remove non rest controller

--- src/main/resources/static/assets/js/theme-listener.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("theme-toggle").addEventListener("click", () => {
-if (document.body.className.includes("dark")) {
-  document.body.classList.remove("dark");
-  localStorage.setItem("pref-theme", "light");
-} else {
-  document.body.classList.add("dark");
-  localStorage.setItem("pref-theme", "dark");
-}
-});
