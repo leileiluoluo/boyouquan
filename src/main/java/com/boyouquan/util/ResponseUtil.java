1c4a47ea10c0a548190d39cffbb99e7d862cdcfc
Jane <Jane@larrys-MacBook-Pro.local>
2024-10-31 17:20:22 +0800

    admin login

--- src/main/java/com/boyouquan/util/ResponseUtil.java
+++ src/main/java/com/boyouquan/util/ResponseUtil.java
@@ -1,17 +1,16 @@
 package com.boyouquan.util;
 
-import com.boyouquan.enumration.ErrorMessage;
+import com.boyouquan.enumration.RestErrorResponse;
 import com.boyouquan.model.ErrorResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseUtil {
 
-    public static ResponseEntity<ErrorResponse> errorResponse(ErrorMessage errorMessage) {
+    public static ResponseEntity<ErrorResponse> errorResponse(RestErrorResponse restErrorResponse) {
         ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setCode(errorMessage.getCode());
-        errorResponse.setMessage(errorMessage.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        errorResponse.setCode(restErrorResponse.getCode());
+        errorResponse.setMessage(restErrorResponse.getMessage());
+        return ResponseEntity.status(restErrorResponse.getHttpStatus())
                 .body(errorResponse);
     }
 
